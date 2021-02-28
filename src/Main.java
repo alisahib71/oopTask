7c47d2993a92f5be7f8d20b8e3d6e657ce810e56
ismayilmammadli <ismayil.mammadli96@gmail.com>
2021-02-28 13:31:56 +0400

    Creating Product

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 import model.Product;
+import service.ProductService;
+import service.impl.ProductServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        Product product = new Product.Builder()
-                .withProductName("HP")
-                .withProductNumber("12xy3z")
-                .withPrice(4000.0)
-                .withCount(3)
-                .build();
+        ProductService productService  = new ProductServiceImpl();
 
-        System.out.println(product);
     }
 }
---
+++ src/dao/ProductDao.java
@@ -0,0 +1,17 @@
+package dao;
+
+import model.Product;
+
+import java.util.List;
+
+public interface ProductDao {
+    void addProduct(Product product);
+
+    Product getProductById(Integer id);
+
+    void deleteProductById(Integer id);
+
+    void updateProductById(Product product, Integer id);
+
+    List<Product> getProductList();
+}
